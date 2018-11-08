59dccd118f6972f16f339891fce478726406bec9
Darren Roscoe <darrenroscoe@gmail.com>
2018-11-08 21:38:25 +0000

    Initial commit, Phoenix generated code

---
+++ lib/task_manager_spa_web/router.ex
@@ -0,0 +1,26 @@
+defmodule TaskManagerSpaWeb.Router do
+  use TaskManagerSpaWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", TaskManagerSpaWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", TaskManagerSpaWeb do
+  #   pipe_through :api
+  # end
+end
