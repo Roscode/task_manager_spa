27b0d2171331a372da5aa8b44a679ca8b139bd16
Darren Roscoe <darrenroscoe@gmail.com>
2018-11-08 19:03:54 -0500

    Add some seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias TaskManagerSpa.Repo
+alias TaskManagerSpa.Users.User
+
+pwhash = Argon2.hash_pwd_salt("passw0rd")
+
+roscode = Repo.insert!(%User{username: "roscode", password_hash: pwhash})
+Repo.insert!(%User{username: "anotheruser", password_hash: pwhash})
+
+alias TaskManagerSpa.Tasks.Task
+
+Repo.insert!(%Task{title: "Do your homework", description: "it's due tomorrow", assignee_id: roscode.id })
+Repo.insert!(%Task{title: "take a break", description: "you deserve it"})
